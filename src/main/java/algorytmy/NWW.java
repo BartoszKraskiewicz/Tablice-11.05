197ab0f947e0861bf6e89b607a215acdeaf2b675
Bartek Kraskiewicz <bartosz.kraskiewicz@gmail.com>
2019-05-12 10:21:59 +0200

    Added NWW

---
+++ src/main/java/algorytmy/NWW.java
@@ -0,0 +1,11 @@
+package algorytmy;
+
+public class NWW {// najmniejsza wspólna wielokrotność
+
+    public static int oblicz(int a, int b){
+        return a * b / NWD.resztaZDzielenia(a, b);
+
+    }
+
+
+}
